a2ef4273686bc322ba76e15aa1497a46dbf1a36b
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-05-13 10:06:08 +0700

    Assign value ke data field , data baru

--- 0209_DoubleLinkedList.cpp
+++ 0209_DoubleLinkedList.cpp
@@ -19,4 +19,6 @@ void addNode()
 {
     //pembuatan node dan pemberian value untuk data noMhs dan name
     Node *newNode = new Node();
+    cout << "\nEnter the roll number of the Student: ";
+    cin >> newNode-> name;
 }
