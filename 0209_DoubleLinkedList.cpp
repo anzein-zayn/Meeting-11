f32f740cc6cd6e220409eed072fd6eeae5f04959
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-05-13 11:07:15 +0700

    membuat pengulangan if jika current sama dengan null

--- 0209_DoubleLinkedList.cpp
+++ 0209_DoubleLinkedList.cpp
@@ -118,4 +118,10 @@ void deleteNode()
         previous = current;
         current =current ->next;
     }
+
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << "not found\033";
+        return;
+    }
 }
